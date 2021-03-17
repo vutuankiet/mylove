79a430368592d84630d7a3cb1b29050478ebf2bc
vutuankiet <75593060+vutuankiet@users.noreply.github.com>
2021-03-17 19:38:21 +0700

    Update love.js

--- mylove/renxi/love.js
+++ mylove/renxi/love.js
@@ -1,4 +1,4 @@
-﻿(function(window){
+(function(window){
 
     function random(min, max) {
         return min + Math.floor(Math.random() * (max - min + 1));
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.fillText("Welcome", 23, 10);
             ctx.restore();
         },
         clear: function() {
